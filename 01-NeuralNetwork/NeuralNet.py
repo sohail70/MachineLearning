0ffd71757706d88859916de2663ee4779811cf8d
sohail70 <sohail.e.nia@gmail.com>
2022-11-11 23:23:03 +0330

    Start NeuralNet

---
+++ 01-NeuralNetwork/NeuralNet.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+
+
+class NeuralNetwork:
+    def __init__(self) -> None:
+        pass
+
+    
+    def relu(x):
+        return np.max([0,x])
+
+
+
+if __name__== "__main__":
+    projectName = "Neural Net"
+    print(f"Project: {projectName }")
